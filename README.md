637f46b1abbfd63105b88c9f648a9aa5b977ca92
aaronconway7 <aaronconway796@gmail.com>
2021-05-23 12:24:24 +0100

    Component Design

--- README.md
+++ README.md
@@ -68,3 +68,8 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `yarn build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+## Further Work
+
+- Close the dropdown when clicking outside of dropdown
+- Close the dropdown on esc key press
--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { createGlobalStyle } from 'styled-components'
+
+import Dropdown from './components/Dropdown'
+
+const assignees = [
+    { name: 'Andy', id: 1 },
+    { name: 'George', id: 2 },
+    { name: 'Clive', id: 3 },
+    { name: 'Peter', id: 4 },
+    { name: 'Cleatus', id: 5 },
+    { name: 'Alex', id: 6 },
+    { name: 'Omar', id: 7 },
+    { name: 'Carl', id: 8 },
+    { name: 'William', id: 9 },
+    { name: 'Donald', id: 10 },
+    { name: 'Dave', id: 11 },
+    { name: 'Jerry', id: 12 },
+    { name: 'Gerrie', id: 13 },
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <LandingPage>
+            <GlobalStyle />
+            <Dropdown
+                label={`Assign Item:`}
+                options={assignees}
+                onChange={(option) => console.log('option', option)}
+                // selectedBg={`#83c503`}
+                // value={{ name: 'William', id: 9 }}
+            />
+        </LandingPage>
+    )
 }
 
-export default App;
+export default App
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    background-color: #eceff1;
+    font-family: Open Sans, Roboto, sans-serif;
+  }
+`
+
+const LandingPage = styled.div`
+    background-color: #eceff1;
+    min-height: 100vh;
+    display: grid;
+    place-items: center;
+    font-size: 18px;
+`
